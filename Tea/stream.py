e2495ca6e8435c3d91019d4f776669ae4d40a92e
824750130 <e824750130@qq.com>
2020-05-18 15:45:57 +0800

    add stream base class

---
+++ Tea/stream.py
@@ -0,0 +1,15 @@
+class BaseStream:
+    def __init__(self, size=1024):
+        self.size = size
+
+    def read(self, size=1024):
+        raise ImportError('read method must be overridden')
+
+    def __len__(self):
+        raise ImportError('__len__ method must be overridden')
+
+    def __next__(self):
+        raise ImportError('__next__ method must be overridden')
+
+    def __iter__(self):
+        return self
