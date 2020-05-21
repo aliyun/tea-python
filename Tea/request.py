de7f07006e386c74b0be7610a26fde73e372c661
824750130 <e824750130@qq.com>
2020-05-21 11:24:25 +0800

    support merge tea model

--- Tea/request.py
+++ Tea/request.py
@@ -1,4 +1,14 @@
 class TeaRequest:
+    _PROPERTY_DEFAULT_MAP = {
+        'query': {},
+        'protocol': 'http',
+        'port': 80,
+        'method': 'GET',
+        'headers': {},
+        'pathname': "",
+        'body': None,
+    }
+
     def __init__(self):
         self.query = {}
         self.protocol = "http"
@@ -8,3 +18,9 @@ class TeaRequest:
         self.pathname = ""
         self.body = None
 
+    def __setattr__(self, key, value):
+        if key in self._PROPERTY_DEFAULT_MAP:
+            if not value:
+                self.__dict__[key] = self._PROPERTY_DEFAULT_MAP[key]
+                return
+        self.__dict__[key] = value
