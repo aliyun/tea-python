c17a8d5db1c5f25c2e16edcc36380a588af39343
824750130 <e824750130@qq.com>
2020-08-21 14:54:49 +0800

    Improve model compatibility

--- setup.py
+++ setup.py
@@ -62,7 +62,6 @@ setup_args = {
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
